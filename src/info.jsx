c95cd748fa692ae5caa1b5ae3be00edd1fdbe388
KawanjotSingh27 <kawanjotsingh177@gmail.com>
2024-10-19 04:35:10 +0530

    info

--- src/info.jsx
+++ src/info.jsx
@@ -30,42 +30,52 @@ function Info() {
         case("Amul-Milk"):
             query="MILK";
             image=amul;
+            description='Amul Milk is a high-quality, pasteurized, and homogenized dairy product known for its purity and rich nutritional value, sourced from the Amul cooperative in India.';
             break;
         case("Lay's-Chile-Limon-Potato-Chips"):
             query="Lay's Chile Limon Potato Chips 9.5 Ounce Bag";
             image=lays;
+            description='Chilli Lemon Lays are crispy potato chips seasoned with a tangy lemon flavor and a spicy kick of chili, offering a zesty and bold snacking experience.';
             break;
         case("English-Oven-Premium-White-Bread"):
             query="WHITE BREAD";
+            description='English Oven White Bread is soft, freshly baked, and made from refined flour, offering a light and fluffy texture perfect for sandwiches and everyday toasts.';
             image=bread;
             break;
         case("Diet-Pepsi"):
             query="DIET PEPSI DRINK MIX";
+            description='Diet Pepsi is a low-calorie, sugar-free cola beverage that delivers the classic Pepsi taste with zero sugar, ideal for calorie-conscious consumers.';
             image=pepsi;
             break;
         case("Quaker-Oats"):
             query="OATS";
+            description='Quaker Oats are 100% whole grain oats, offering a nutritious, fiber-rich breakfast option that supports heart health and provides long-lasting energy.';
             image=oats;
             break;
         case("Kellog's-Corn-Flakes"):
             query="CORN FLAKES";
+            description="Kellogg's Corn Flakes are crispy, golden flakes made from lightly toasted corn, providing a delicious and nutritious breakfast rich in essential vitamins and minerals."
             image=corn_flakes;
             break;
         case("Muscleblaze-Biozyme-Whey-Protein"):
             query="PROTEIN POWDER";
             image=protein;
+            description='MuscleBlaze Whey Protein is a high-quality protein supplement designed to support muscle growth and recovery, packed with essential amino acids and fast-digesting whey.';
             break;
         case("Nutri-Choice-Biscuit"):
             query="BISCUIT";
             image=biscuit;
+            description='Nutri Choice Biscuits are wholesome, fiber-rich snacks made with nutritious ingredients, offering a healthy and tasty option for guilt-free snacking.';
             break;
         case("Baskin-Robbins-Very-Berry-Strawberry"):
             query="ICE CREAM";
             image=ice_cream;
+            description='BR (Baskin-Robbins) Ice Cream offers a wide variety of rich, creamy, and deliciously crafted flavors, making it a beloved treat for ice cream lovers worldwide.';
             break;
         case("Aashirvaad-Wheat-Flour"):
             query="WHEAT FLOUR";
             image=flour;
+            description='Ashirvaad Wheat Flour is a premium quality, finely milled flour made from 100% whole wheat grains, ensuring superior taste and nutrition for your culinary creations.';
             break;
     }
 
@@ -124,6 +134,7 @@ function Info() {
                     <div>
                         <p className='font-medium text-3xl'>{name}</p>
                         <p className='text-lg'>{category}</p>
+                        <p className='font-light'>{description}</p>
                     </div>
                     <div className='ml-94'>
                         <p className='text-white bg-purple-600 p-1 rounded-t-lg text-sm'>Health Score</p>
@@ -135,7 +146,15 @@ function Info() {
                     <div className='bg-purple-300 p-5 w-1/2 rounded-xl shadow-sm'>
                         <div className='flex gap-2 flex-col mb-4'>
                             <p className='text-3xl'>Health Rating</p>
-                            <p className='bg-yellow-200 w-14 h-14 text-center p-3 text-xl rounded-full'>70</p>
+                            <div className='flex gap-40 items-center'>
+                                <div className='flex'>
+                                    <div className='bg-red-600 h-6 w-16 rounded-lg'></div>
+                                    <div className='bg-orange-400 h-6 w-16 rounded-lg -ml-2'></div>
+                                    <div className='bg-yellow-400 h-6 w-16 rounded-lg -ml-2'></div>
+                                    <div className='bg-green-400 h-6 w-16 rounded-lg -ml-2'></div>
+                                </div>
+                                <p className='bg-yellow-200 w-16 h-16 text-center p-3 text-3xl rounded-full'>70</p>
+                            </div>
                         </div>
                         <div className='flex flex-col gap-2'>
                             <p className='text-3xl'>Ingredients</p>
